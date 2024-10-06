fc541d7f47df861007e9433d123d76b46f29b14a
Mahmoud Faour <leedor97@gmail.com>
2024-10-06 13:35:30 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Enable static export
-    basePath: '/your-repo-name', // Set the GitHub repository name as the base path
-    assetPrefix: '/your-repo-name/', // Prefix for assets (styles, scripts, etc.)
-  }
-  
-  module.exports = nextConfig
+const nextConfig = {};
+
+export default nextConfig;
